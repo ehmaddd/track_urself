747ca1551b68e3cbdfe4ffafdf2390c30367db65
ehmaddd <ehmaddd@gmail.com>
2024-10-08 17:43:30 +0500

    Adjust weather

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import weatherReducer from './weatherSlice';
-import prayerReducer from './prayerSlice';
 
 const store = configureStore({
   reducer: {
     weather: weatherReducer,
-    prayer: prayerReducer,
   }
 });
 
