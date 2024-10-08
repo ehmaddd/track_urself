a799e26b7eed2ec492f052bef92a21c911ceba7c
ehmaddd <ehmaddd@gmail.com>
2024-10-08 16:02:59 +0500

    Change Store and Login

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,12 +1,41 @@
-import { configureStore } from '@reduxjs/toolkit';
-import weatherReducer from './weatherSlice';
-import prayerReducer from './prayerSlice';
+import { createStore } from 'redux';
 
-const store = configureStore({
-  reducer: {
-    weather: weatherReducer,
-    prayer: prayerReducer,
+// Initial state with pre-filled users
+const initialState = {
+  users: [
+    { username: 'john_doe', password: 'password123' },
+    { username: 'jane_smith', password: 'password456' },
+  ],
+  loggedInUser: null,
+};
+
+// Reducer to manage user-related actions
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'REGISTER_USER':
+      return {
+        ...state,
+        users: [...state.users, action.payload], // Add new user to the state
+      };
+    case 'LOGIN_USER':
+      const user = state.users.find(
+        (user) =>
+          user.username === action.payload.username &&
+          user.password === action.payload.password
+      );
+      if (user) {
+        return { ...state, loggedInUser: user.username };
+      } else {
+        return { ...state, loggedInUser: null };
+      }
+    case 'LOGOUT_USER':
+      return { ...state, loggedInUser: null };
+    default:
+      return state;
   }
-});
+};
+
+// Create Redux store
+const store = createStore(userReducer);
 
 export default store;
